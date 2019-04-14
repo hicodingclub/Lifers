fe7b829573eb6f064e216586fe40f2215f42abea
Eric Sheng <ericsheng@Erics-MacBook-Pro.local>
2019-04-14 02:30:01 -0700

    add

---
+++ front-end/src/app/allHacks/public-routing.ts
@@ -0,0 +1,5 @@
+import { EventDetailComponent } from '../publicinfo/event/event-detail/event-detail.component';
+
+export const publicRoutingPath = [
+     {path: 'event/:id', component: EventDetailComponent},
+];
